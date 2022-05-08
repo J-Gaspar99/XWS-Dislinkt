c8c4a4ec76349d48d9e8a9c258f21ce269b0d13d
J-Gaspar99 <64786333+J-Gaspar99@users.noreply.github.com>
2022-05-08 23:22:08 +0200

    user/update

---
+++ followings/followings.go
@@ -0,0 +1,110 @@
+package followings
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type Followings struct {
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	FollowerID  primitive.ObjectID `json:"followerID,omitempty" bson:"followerID,omitempty"`
+	FollowingID primitive.ObjectID `json:"followingID,omitempty" bson:"followingID,omitempty"`
+}
+
+//Get 1 by ID
+func GetFollowingByIDEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var following Followings
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	err := collection1.FindOne(ctx, Followings{ID: id}).Decode(&following)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(following)
+}
+
+//Get 1 by FollowingID
+func GetFollowingByFollowingIDEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	followingID, _ := primitive.ObjectIDFromHex(params["followingID"])
+	var following Followings
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	err := collection1.FindOne(ctx, Followings{FollowingID: followingID}).Decode(&following)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(following)
+}
+
+//get all by userID
+func GetFollowingsByFollowingIDEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	followingID, _ := primitive.ObjectIDFromHex(params["followingID"])
+	var following []Followings
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	cursor, err := collection1.Find(ctx, Followings{FollowingID: followingID})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	if err = cursor.All(ctx, &following); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(following)
+}
+
+//Create 1
+func CreateFollowingEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	var followings Followings
+	json.NewDecoder(request.Body).Decode(&followings)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	result, _ := collection1.InsertOne(ctx, followings)
+	json.NewEncoder(response).Encode(result)
+}
+
+//Get all
+func GetFollowingsEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+
+	var followings []Followings
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	cursor, err := collection1.Find(ctx, bson.M{})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	if err = cursor.All(ctx, &followings); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(followings)
+}
+
+var collection1 *mongo.Collection
+
+func Handle(client *mongo.Client) {
+
+	collection1 = client.Database("XML").Collection("followings")
+
+}
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+// ExClM5mbC5jclJyA
+import (
+	"XML/followRequests"
+	"XML/followings"
+	"XML/posts"
+	"XML/user"
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+var collection *mongo.Collection
+
+func main() {
+
+	fmt.Println("Pokretanje...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://admin:ExClM5mbC5jclJyA@cluster.ihwbw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	if err != nil {
+		panic(err)
+
+	}
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	user.Handle(client)
+	posts.Handle(client)
+	followings.Handle(client)
+	followRequests.Handle(client)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/posts", posts.GetPostsEndpoint).Methods("GET")
+	router.HandleFunc("/post/{id}", posts.GetPostByIDEndpoint).Methods("GET")
+	router.HandleFunc("/posts/userid/{userid}", posts.GetPostsByUserIDEndpoint).Methods("GET")
+	router.HandleFunc("/posts", posts.CreatePostEndpoint).Methods("POST")
+	//router.HandleFunc("/posts/search", posts.SearchPostsEndpoint).Methods("GET")
+
+	router.HandleFunc("/users", user.GetUsersEndpoint).Methods("GET")
+	router.HandleFunc("/user/{id}", user.GetUserByIDEndpoint).Methods("GET")
+	//router.HandleFunc("/user/search", user.SearchUsersEndpoint).Methods("GET")
+	router.HandleFunc("/users", user.RegiterUserEndpoint).Methods("POST")
+	router.HandleFunc("/user/login/{username}/{password}", user.LoginUserEndpoint).Methods("GET")
+	router.HandleFunc("/user/update/{id}", user.UpdateUserEndpoint).Methods("PUT")
+
+	router.HandleFunc("/followings", followings.GetFollowingsEndpoint).Methods("GET")
+	router.HandleFunc("/following/{id}", followings.GetFollowingByIDEndpoint).Methods("GET")
+	router.HandleFunc("/following/fing/{followingID}", followings.GetFollowingByFollowingIDEndpoint).Methods("GET")
+	router.HandleFunc("/followings/fings/{followingID}", followings.GetFollowingByFollowingIDEndpoint).Methods("GET")
+	router.HandleFunc("/followings", followings.CreateFollowingEndpoint).Methods("POST")
+
+	router.HandleFunc("/followrequests", followRequests.GetFollowRequestsEndpoint).Methods("GET")
+	router.HandleFunc("/followrequest/{id}", followRequests.GetFollowRequestByIDEndpoint).Methods("GET")
+	router.HandleFunc("/followrequests", followRequests.CreateFollowRequestEndpoint).Methods("POST")
+
+	http.ListenAndServe(":12345", router)
+}
