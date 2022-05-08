08ecc5d90a8a9e3f59d5b0c5b980592686f314fe
davidegelja <64745304+davidegelja@users.noreply.github.com>
2022-05-08 16:10:10 +0200

    Posts fix, followings and followRequests added

--- main.go
+++ main.go
@@ -2,75 +2,19 @@ package main
 
 // ExClM5mbC5jclJyA
 import (
+	"XML/followRequests"
+	"XML/followings"
+	"XML/posts"
 	"XML/user"
 	"context"
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/mux"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-//////////////////////STRUKTURE ZA RAD SAD PODACIMA
-
-type Posts struct {
-	ID       int64  `json:"id,omitempty" bson:"id,omitempty"`
-	FullText string `json:"text,omitempty" bson:"text,omitempty"`
-	User     struct {
-		UserName string `json:"username" bson:"username"`
-	} `json:"user,omitempty" bson:"user,omitempty"`
-}
-
-func GetPostsEndpoint(response http.ResponseWriter, request *http.Request) {
-	response.Header().Set("content-type", "application/json")
-
-	var posts []Posts
-	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	cursor, err := collection.Find(ctx, bson.M{})
-	if err != nil {
-		response.WriteHeader(http.StatusInternalServerError)
-		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
-		return
-	}
-	if err = cursor.All(ctx, &posts); err != nil {
-		response.WriteHeader(http.StatusInternalServerError)
-		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
-		return
-	}
-	json.NewEncoder(response).Encode(posts)
-}
-func SearchPostsEndpoint(response http.ResponseWriter, request *http.Request) {
-	response.Header().Set("content-type", "application/json")
-	queryParams := request.URL.Query()
-	var posts []Posts
-	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	searchStage := bson.D{
-		{"$search", bson.D{
-			{"index", "synsearch"},
-			{"text", bson.D{
-				{"query", queryParams.Get("q")},
-				{"path", "text"},
-				{"synonyms", "posts"},
-			}},
-		}},
-	}
-	cursor, err := collection.Aggregate(ctx, mongo.Pipeline{searchStage})
-	if err != nil {
-		response.WriteHeader(http.StatusInternalServerError)
-		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
-		return
-	}
-	if err = cursor.All(ctx, &posts); err != nil {
-		response.WriteHeader(http.StatusInternalServerError)
-		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
-		return
-	}
-	json.NewEncoder(response).Encode(posts)
-}
 
 var client *mongo.Client
 var collection *mongo.Collection
@@ -91,21 +35,26 @@ func main() {
 			panic(err)
 		}
 	}()
-	collection = client.Database("XML").Collection("posts")
 
 	user.Handle(client)
-
-	//collection1 = client.Database("XML").Collection("users")
+	posts.Handle(client)
+	followings.Handle(client)
+	followRequests.Handle(client)
 
 	router := mux.NewRouter()
-	router.HandleFunc("/posts", GetPostsEndpoint).Methods("GET")
-	router.HandleFunc("/posts/search", SearchPostsEndpoint).Methods("GET")
+
+	router.HandleFunc("/posts", posts.GetPostsEndpoint).Methods("GET")
+	//router.HandleFunc("/posts/search", posts.SearchPostsEndpoint).Methods("GET")
 
 	router.HandleFunc("/users", user.GetUsersEndpoint).Methods("GET")
 	router.HandleFunc("/user/search", user.SearchUsersEndpoint).Methods("GET")
 	router.HandleFunc("/user/register", user.RegiterUserEndpoint).Methods("POST")
 	router.HandleFunc("/user/login", user.LoginUserEndpoint).Methods("POST")
 	router.HandleFunc("/user/update", user.UpdateUserEndpoint).Methods("PUT")
+
+	router.HandleFunc("/followings", followings.GetFollowingsEndpoint).Methods("GET")
+
+	router.HandleFunc("/followrequests", followRequests.GetFollowRequestsEndpoint).Methods("GET")
 
 	http.ListenAndServe(":12345", router)
 }
---
+++ posts/posts.go
@@ -0,0 +1,78 @@
+package posts
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type Posts struct {
+	ID       int64  `json:"id,omitempty" bson:"id,omitempty"`
+	FullText string `json:"text,omitempty" bson:"text,omitempty"`
+	Likes    int32  `json:"like,omitempty" bson:"like,omitempty"`
+	Dislike  int32  `json:"dislike,omitempty" bson:"dislike,omitempty"`
+	User     struct {
+		UserName string `json:"username" bson:"username"`
+	} `json:"user,omitempty" bson:"user,omitempty"`
+}
+
+func GetPostsEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+
+	var posts []Posts
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	cursor, err := collection1.Find(ctx, bson.M{})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	if err = cursor.All(ctx, &posts); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(posts)
+}
+
+/*
+func SearchPostsEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	queryParams := request.URL.Query()
+	var posts []Posts
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	searchStage := bson.D{
+		{"$search", bson.D{
+			{"index", "synsearch"},
+			{"text", bson.D{
+				{"query", queryParams.Get("q")},
+				{"path", "text"},
+				{"synonyms", "posts"},
+			}},
+		}},
+	}
+	cursor, err := collection1.Aggregate(ctx, mongo.Pipeline{searchStage})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	if err = cursor.All(ctx, &posts); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(posts)
+}
+*/
+var collection1 *mongo.Collection
+
+func Handle(client *mongo.Client) {
+
+	collection1 = client.Database("XML").Collection("posts")
+
+}
