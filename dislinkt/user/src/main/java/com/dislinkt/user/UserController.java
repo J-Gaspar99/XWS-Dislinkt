49cab163679b1f96f53275438463ea0427f67aad
davidegelja <64745304+davidegelja@users.noreply.github.com>
2022-06-30 15:46:33 +0200

    User crud

--- dislinkt/user/src/main/java/com/dislinkt/user/UserController.java
+++ dislinkt/user/src/main/java/com/dislinkt/user/UserController.java
@@ -1,46 +1,61 @@
 package com.dislinkt.user;
 
 
+import com.dislinkt.user.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 import java.util.Optional;
 
 
 @RestController
-//@RequestMapping("api/v1/users")  //on ovo nema videcemo po potrebi
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
     //CREATE
-    @PostMapping("/createUser")
+    @PostMapping("/user")
     public String createUser(@RequestBody User user){
         userRepository.save(user);
         return "Created user with id: " + user.getId();
     }
 
     //FIND ALL
-    @GetMapping("/findAllUsers")
+    @GetMapping("/user")
     public List<User> getUsers(){
         return userRepository.findAll();
     }
 
     //FIND BY ID
-    @GetMapping("/findById/{id}")
+    @GetMapping("/user/{id}")
     public Optional<User> getUser(@PathVariable int id){
         return userRepository.findById(id);
     }
 
     //DELETE
-    @DeleteMapping("/deleteUser/{id}")
+    @DeleteMapping("/user/{id}")
     public String deleteuser(@PathVariable int id){
         userRepository.deleteById(id);
         return "Deleted user with id: " + id;
     }
 
 
+    //UPDATE
+    @PutMapping("/user/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Integer id, @RequestBody User userDetails){
+        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User does not exist with id:"+ id));
+
+        user.setFirstName(userDetails.getFirstName());
+        user.setLastName(userDetails.getLastName());
+        user.setEmail(userDetails.getEmail());
+
+
+        User updatedUser = userRepository.save(user);
+        return ResponseEntity.ok(updatedUser);
+    }
 }
 
