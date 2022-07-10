b19a1f65eb05e4fa26b896ec1fd3619bb095d95d
davidegelja <64745304+davidegelja@users.noreply.github.com>
2022-07-10 18:02:48 +0200

    -dislinkt -agent

--- dislinkt/followRequest/src/main/java/com.dislinkt.followRequest/FollowRequestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dislinkt.followRequest;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FollowRequestRepository extends MongoRepository<FollowRequest,Integer> {
-    FollowRequest findByFollowerId(Integer follower);
-    FollowRequest findByFollowingId(Integer following);
-}
--- dislinkt/follows/src/main/java/com/dislinkt/follows/FollowsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dislinkt.follows;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FollowsRepository extends MongoRepository<Follows,Integer> {
-    Follows findByFollowerId(Integer follower);
-    Follows findByFollowingId(Integer following);
-}
